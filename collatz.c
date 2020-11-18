8a1a89cbed5f83b381bd81966881f56434421ada
hanatsu-1404 <m20028@g.metro-cit.ac.jp>
2020-11-18 20:06:50 +0900

    test-commit

---
+++ collatz.c
@@ -0,0 +1,22 @@
+﻿#include<stdio.h>
+int main(void){
+    int n;
+    int count = 0;
+    printf("input:");
+    scanf("%d",&n);
+    printf("---------\n");
+    while(1){
+        if(n==1){
+            break;
+        }else if(n%2==0){
+            n/=2;
+            count++;
+            printf("%d/%d\n---------\n",n,count);
+        }else{
+            n = n*3+1;
+            count++;
+            printf("%d/%d\n---------\n",n,count);
+        }
+    }
+    printf("count:%d",count);
+}
